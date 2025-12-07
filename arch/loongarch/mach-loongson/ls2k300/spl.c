7b564cad5ab07064617d6bff6dc6f545b9044a65
Ilikara <3435193369@qq.com>
2025-12-08 02:17:56 +0800

    drop weird gmac-phy reset quirk in 2k300/spl.c
    Signed-off-by: Ilikara <3435193369@qq.com>

--- arch/loongarch/mach-loongson/ls2k300/spl.c
+++ arch/loongarch/mach-loongson/ls2k300/spl.c
@@ -4,50 +4,11 @@
 #include <asm/io.h>
 #include <asm/addrspace.h>
 #include <mach/loongson.h>
-#include <common.h>
-#include <command.h>
-#include <env.h>
-#include <env_internal.h>
-#include <flash.h>
-#include <init.h>
-#include <led.h>
-#include <log.h>
-#include <malloc.h>
-#include <net.h>
-#include <spi.h>
-#include <spi_flash.h>
-#include <linux/delay.h>
-#include <linux/stringify.h>
-#include <u-boot/crc.h>
-#include <uuid.h>
-#include <linux/ctype.h>
 #include <linux/io.h>
 
 void spl_mach_init(void)
 {
 	arch_cpu_init();
-}
-
-static void setup_gpio_value(unsigned int gpio, int value)
-{
-	unsigned char val;
-	if (gpio > LS_GPIO_MAX) {
-		printf("Error: setup_gpio_value: gpio(%d) not exist!\n", gpio);
-		return;
-	}
-	val = value ? 1 : 0;
-	readb(LS_GPIO_B_DIR_BASE + gpio) = 0x0;
-	readb(LS_GPIO_B_OUT_BASE + gpio) = val;
-}
-
-void spl_board_init_late(void)
-{
-	if (!strcmp(CONFIG_DEFAULT_DEVICE_TREE, "ls2k300_pai"))
-		setup_gpio_value(75, 1);
-	else if (!strcmp(CONFIG_DEFAULT_DEVICE_TREE, "ls2k300_atk_dl2k0300b_v01")) {
-		setup_gpio_value(72, 1);	//phy0 output high
-		setup_gpio_value(73, 1);	//phy1 output high
-	}
 }
 
 void spl_mach_init_late(void)
