dac50d6f4aee3c6058230c42380f75c952e35205
Ilikara <3435193369@qq.com>
2025-12-07 22:26:20 +0800

    use aosc's gcc-cross
    Signed-off-by: Ilikara <3435193369@qq.com>

--- set_env.sh
+++ set_env.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 #set -x
-
-abi1_toolchain_list=("/opt/loongson-gnu-toolchain-x86_64-loongarch64-linux-gnu" \
-					"/opt/loongson-gnu-toolchain-8.3-x86_64-loongarch64-linux-gnu-rc1.4" \
-					"/opt/loongson-gnu-toolchain-8.3-x86_64-loongarch64-linux-gnu-rc1.3-1" \
-					"/opt/loongson-gnu-toolchain-8.3-x86_64-loongarch64-linux-gnu-rc1.2" \
-					"/opt/toolchain-loongarch64-linux-gnu-gcc8-host-x86_64-2022-07-18")
-
-abi2_toolchain_list=("/opt/loongarch64-linux-gnu-gcc13.3" \
-					"/opt/loongarch64-linux-gnu-gcc14.2")
+abi2_toolchain_list=("/opt/abcross/loongarch64")
 
 real_target=0
 real_toolchain=""
@@ -20,13 +12,13 @@ function setup_loongarch_env()
 	CC_PREFIX=$real_toolchain
 	export PATH=$PATH:$CC_PREFIX/bin
 	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CC_PREFIX/lib
-	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CC_PREFIX/loongarch64-linux-gnu/lib64
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CC_PREFIX/loongarch64-aosc-linux-gnu/lib
 
 	export ARCH=loongarch
-	export CROSS_COMPILE=loongarch64-linux-gnu-
+	export CROSS_COMPILE=loongarch64-aosc-linux-gnu-
 }
 
-for i in ${abi1_toolchain_list[*]}
+for i in ${abi2_toolchain_list[*]}
 do
 	real_toolchain=$i
 	if [ -d $real_toolchain ]; then
@@ -34,21 +26,6 @@ do
 		break;
 	fi
 done
-
-if [ $# -eq 1 ] ; then
-	if [[ "$1" == "abi2" ]]; then
-		real_target=0
-		real_toolchain=""
-		for i in ${abi2_toolchain_list[*]}
-		do
-			real_toolchain=$i
-			if [ -d $real_toolchain ]; then
-				real_target=1
-				break;
-			fi
-		done
-	fi
-fi
 
 if [ $real_target -eq 0 ]; then
 	echo "没有找到相关工具链的文件夹"
